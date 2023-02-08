d609b4f35bd8033d83c93d71d322ef777ea0a13f
Geoff Fox <fox.geoffrey@gmail.com>
2023-02-08 14:11:39 -0800

    Add cors

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 /* eslint-disable no-console */
 const express = require('express');
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const cors = require('cors');
 const Book = require('./models/bookModel');
 
 const app = express();
+// cors
+// app.use(cors);
+
 /* Used to fix warning */
 mongoose.set('strictQuery', false);
 
@@ -18,14 +23,20 @@ const bookRouter = express.Router();
 const port = process.env.PORT || 3001;
 
 /* http://localhost:4201/api/books */
-bookRouter.route('/books').get((req, res) => {
-  Book.find((err, books) => {
-    if (err) {
-      return res.send(err);
+bookRouter.route('/books')
+  .get(cors(), (req, res) => {
+    const query = {};
+    if (req.query.genre) {
+      query.genre = req.query.genre;
     }
-    return res.json(books);
+    Book.find(query, (err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
   });
-});
+
 app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
